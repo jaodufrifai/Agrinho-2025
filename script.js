e8c347947f0e71d259bfe7e9c30ab43a9ee633de
João Costa <148370307+jaodufrifai@users.noreply.github.com>
2025-06-24 02:04:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-
-function aumentarfont(){
-    var elemento = document.querySelector("body");
-    var tamanhoAtual = window.getComputedStyle(elemento).fontSize;
-    var novoTamanho = parseFloat(tamanhoAtual) + 2 + "px";
-    elemento.style.fontSize = novoTamanho;
-}
-
-function diminuirfont(){
-    var elemento = document.querySelector("body");
-    var tamanhoAtual = window.getComputedStyle(elemento).fontSize;
-    var novoTamanho = parseFloat(tamanhoAtual) - 2 + "px";
-    elemento.style.fontSize = novoTamanho;
-}
-
 function toggleSenha() {
     var senhaInput = document.getElementById("senhaTexto");
     if (senhaInput.type === "password") {
@@ -26,12 +11,3 @@ document.getElementById("toggleSenha").addEventListener("click", function() {
     var senhaInput = document.getElementById("senha");
     senhaInput.type = senhaInput.type === "password" ? "text" : "password";
 })
-
-function toggleDropdown() {
-  var dropdown = document.getElementById("dropdown");
-  dropdown.style.display = (dropdown.style.display === "none") ? "block" : "none";
-}
-
-function alternarContraste() {
-    document.body.classList.toggle("alto-contraste");
-}
